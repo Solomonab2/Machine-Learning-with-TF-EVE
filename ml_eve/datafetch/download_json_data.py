2d4323afc0dce27ca9ac62964dbf0a6d89205b67
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-08 18:45:35 +0000

    Adding tarfile member sanitization to extractall()

--- ml_eve/datafetch/download_json_data.py
+++ ml_eve/datafetch/download_json_data.py
@@ -79,7 +79,26 @@ def download(download_year, download_month, download_first_day, number_of_days):
                     tarinfo for tarinfo in tar.getmembers()
                     if tarinfo.name.startswith("killmails/")
                 ]
-                tar.extractall(members=subdir_and_files, path=directory_name)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, members=subdir_and_files, path=directory_name)
 
             """Delete the downloaded .tar files"""
             if exists(file_path):
